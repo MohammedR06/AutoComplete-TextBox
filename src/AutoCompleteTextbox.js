e4a01d3ca2a0110aac2313ab1a2a5c7160c7696c
MohammedR06 <mohdradhan06@gmail.com>
2023-03-28 16:08:02 +0530

    changes

--- src/AutoCompleteTextbox.js
+++ src/AutoCompleteTextbox.js
@@ -6,56 +6,61 @@ function AutoCompleteTextbox() {
   const [agent, setAgent] = useState([]);
   const [nameMatch, setNameMatch] = useState("");
   const [suggestion, setSuggestion] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchErr, setSearchErr] = useState(false);
   useEffect(() => {
     const loadName = async () => {
       const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/comments`
+        `http://62.252.239.190:9020/v1/agents/s`
       );
-      //   console.log(response.data);
+      console.log(response.data);
       setAgent(response.data);
     };
     loadName();
   }, []);
   //   console.log(agent);
+
   const onTextBox = (nameMatch) => {
     let matches = [];
     if (nameMatch.length > 0) {
       matches = agent.filter((user) => {
         const regex = new RegExp(`${nameMatch}`, "gi");
-        return user.email.match(regex);
+        return user.name.match(regex);
       });
     }
     // console.log("matches", matches);
     setSuggestion(matches);
     setNameMatch(nameMatch);
   };
+
   return (
     <>
       <div className="conatiner pt-2" id="rcorners2">
         <div className="wrap">
-          <h3 className="">
+          <h3 style={{ color: "#223461" }}>
             <i className="fa-solid fa-user" style={{ margin: "0 15px" }}></i>
             Agent Details
           </h3>
           <div className="search">
             <input
+              className="form-control mx-5 "
+              id="inputName"
               type="text"
-              className="searchTerm"
               placeholder="Search Agent"
+              aria-label="default input example"
               value={nameMatch}
               onChange={(e) => {
                 onTextBox(e.target.value);
               }}
+              required
             />
-            <button type="submit" className="searchButton">
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
+
             {suggestion &&
               suggestion.map((suggestion, i) => {
                 return (
-                  <ul id="myUL" className="mx-5 px-5">
+                  <ul key={i} id="myUL" className="mx-5 px-4">
                     <li>
-                      <a style={{ fontSize: "14px" }}>{suggestion.email}</a>
+                      <a style={{ fontSize: "14px" }}>{suggestion.name}</a>
                     </li>
                   </ul>
                 );
